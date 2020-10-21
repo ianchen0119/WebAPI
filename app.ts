4bcd4e21e2c90db02b3233e8f50b0c6f7f5c1709
Ian Chen <iancodinghtml@gmail.com>
2020-10-21 19:40:10 +0800

    [UPDATE] CORS

--- app.ts
+++ app.ts
@@ -1,10 +1,11 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import { UserRoutes } from "./routers/Route.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 
 const app = new Application();
 const router = new Router();
 const userRoutes = UserRoutes(router);
-
+app.use(oakCors()); // Enable CORS for All Routes
 app.use(userRoutes.routes());
 app.use(userRoutes.allowedMethods());
 
